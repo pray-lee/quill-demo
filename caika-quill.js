6f8b147ec81e6294f00a345ea226611efa2b0676
pray-lee <praycis@gmail.com>
2019-11-07 17:10:11 +0800

    update

--- caika-quill.js
+++ caika-quill.js
@@ -58,42 +58,14 @@ Quill.register('modules/counter', function (quill, options) {
 var Size = Quill.import('attributors/style/size');
 Size.whitelist = ['12px', '13px', '14px', '15px', '16px', '18px', '20px', '22px', '24px'];
 Quill.register(Size, true);
-var editor = new Quill('#editor', {
-    modules: {
-        toolbar: toolbarOptions,
-        counter: true, //计数
-    },
-    theme: 'snow',
-    placeholder: '请输入一些内容',
-    readOnly: false,
-    scrollingContainer: null // 指定该容器具有滚动条。
-})
-document.getElementsByClassName('ql-image')[0].setAttribute('id', 'ql-image')
-// 事件处理程序 处理图片上传
-var toolbar = editor.getModule('toolbar')
-var CKinstance = new CKEditor('ql-image')
-var uploader = CKinstance.createUploader(editor)
-toolbar.addHandler('image', function () {
-    CKEditor.set_upload_param(uploader, '', false)
-})
 
-var editor1 = new Quill('#editor1', {
-    modules: {
-        toolbar: toolbarOptions,
-        counter: true, //计数
-    },
-    theme: 'snow',
-    placeholder: '请输入一些内容',
-    readOnly: false,
-    scrollingContainer: null // 指定该容器具有滚动条。
-})
-
-// 给image按钮加一个id, 供upload.js使用
-document.getElementsByClassName('ql-image')[1].setAttribute('id', 'ql-image1')
-
-var toolbar1 = editor1.getModule('toolbar')
-var CKinstance1 = new CKEditor('ql-image1')
-var uploader1 = CKinstance1.createUploader(editor1)
-toolbar1.addHandler('image', function () {
-    CKEditor.set_upload_param(uploader1, '', false)
-})
+// 绑定图片处理程序, 用来调用upload.js里面的功能
+function bundleImageClick(eleId,editor) {
+    document.getElementById(eleId).getElementsByClassName('ql-image')[0].setAttribute('id', 'ql-image' + eleId)
+    var toolbar = editor.getModule('toolbar')
+    var CKinstance = new CKEditor('ql-image' + eleId)
+    var uploader = CKinstance.createUploader(editor)
+    toolbar.addHandler('image', function () {
+        CKEditor.set_upload_param(uploader, '', false)
+    })
+}
